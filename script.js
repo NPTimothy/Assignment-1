d4b2c2f5fa0d2a814e8dfb9ddc021719fdc7d45d
Timothy Chai <s10256132@connect.np.edu.sg>
2023-12-01 01:28:47 +0800

    Moved scripts to bottom of HTML, added validation of coupons in script.js

--- script.js
+++ script.js
@@ -5,3 +5,43 @@ window.onscroll = function () {
         document.querySelector("nav").classList.remove("scroll");
     }
 };
+
+let coupons = ["THL", "TEST"];
+
+function validateCoupon(coupons) {
+    let couponMessage = document.getElementsByClassName("coupon-message")[0];
+    let coupon = document
+        .getElementsByClassName("coupon")[0]
+        .value.toUpperCase();
+    if (coupons.includes(coupon)) {
+        couponMessage.innerHTML =
+            "Coupon applied successfully. Enjoy your discount!";
+        document
+            .getElementsByClassName("coupon-message")[0]
+            .classList.add("successful");
+        document
+            .getElementsByClassName("coupon-message")[0]
+            .classList.remove("error");
+    } else if (coupon === "") {
+        couponMessage.innerHTML = "";
+    } else {
+        couponMessage.innerHTML = "Invalid coupon code entered.";
+        document
+            .getElementsByClassName("coupon-message")[0]
+            .classList.add("error");
+        document
+            .getElementsByClassName("coupon-message")[0]
+            .classList.remove("successful");
+    }
+    // console.log(coupon);
+}
+
+let couponInput = document.getElementsByClassName("coupon")[0];
+
+couponInput.addEventListener(
+    "keyup",
+    function () {
+        validateCoupon(coupons);
+    },
+    false
+);
